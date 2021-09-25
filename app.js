62ad0f42accf73d0bf89c3f10ac6de653674380b
Vipul R Girme <49040675+VipulGirme@users.noreply.github.com>
2021-09-25 16:53:52 +0530

    Minion Text Ready

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!");
-    console.log("Input , "+txtInput.value);
-    console.log( + " -- " + outputDiv);
-});
+function getTransaltion(text) {
+    return serverUrl+ "?"+"text="+text;
+}
+
+function clickHandler() {
+
+    var textInput = txtInput.value;
+    console.log(textInput);
+    fetch(getTransaltion(textInput))
+    .then(res => (res.json())
+    .then(json => {
+        outputDiv.innerText = json.contents.translated
+    }));
+}
+
+btnTranslate.addEventListener("click", clickHandler);
